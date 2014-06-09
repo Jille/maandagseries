0957aa03d0432d2bbe9c3e173abeb4d835090671
Jille Timmermans <jille@karpenoktem.nl>
2014-06-09 14:20:43 +0200

    Add -f to force picking a chef and souschef

--- humanpicker/management/commands/draw.py
+++ humanpicker/management/commands/draw.py
@@ -1,5 +1,6 @@
 import datetime
 import random
+from optparse import make_option
 
 from django.core.management.base import BaseCommand, CommandError
 from django.core.mail import EmailMessage
@@ -18,8 +19,15 @@ def pick_one_from_scores(scores):
 	offByOneBug()
 
 class Command(BaseCommand):
-	args = ''
+	args = '[-f]'
 	help = 'Draw some random names'
+	option_list = BaseCommand.option_list + (
+		make_option('-f',
+		action='store_true',
+		dest='forcePick',
+		default=False,
+		help='Force picking the chef and souschef'),
+	)
 
 	def handle(self, *args, **options):
 		if len(args) != 0:
@@ -27,56 +35,66 @@ class Command(BaseCommand):
 		for ev in Event.objects.filter(open=True):
 			already_accepted = map(lambda x: x.name, Human.objects.filter(event=ev, accepted=True, revoked=False))
 			places = ev.places - len(already_accepted)
-			if places <= 0:
-				continue
-			humans = dict((x.id, x) for x in (Human.objects.filter(event=ev, accepted=False, revoked=False)))
-			if len(humans) == 0:
-				continue
-			scores = {}
-			for hid, human in humans.items():
-				scores[hid] = 100
-				if human.is_creative:
-					scores[hid] += 25
 			new_names = []
-			while places > 0 and len(scores) > 0:
-				hid = pick_one_from_scores(scores)
-				humans[hid].accepted = True
-				humans[hid].save()
-				del scores[hid]
-				places -= 1
-				new_names.append(humans[hid].name)
-			if places == 0 and ev.chef is None:
-				chefs = Human.objects.filter(event=ev, accepted=True, revoked=False, is_creative=False)
+			mailworthy = False
+			if places > 0:
+				humans = dict((x.id, x) for x in (Human.objects.filter(event=ev, accepted=False, revoked=False)))
+				if len(humans) > 0:
+					scores = {}
+					for hid, human in humans.items():
+						scores[hid] = 100
+						if human.is_creative:
+							scores[hid] += 25
+					while places > 0 and len(scores) > 0:
+						hid = pick_one_from_scores(scores)
+						humans[hid].accepted = True
+						humans[hid].save()
+						del scores[hid]
+						places -= 1
+						new_names.append(humans[hid].name)
+						mailworthy = True
+			if (places <= 0 or options['forcePick']) and ev.chef is None:
+				chefs = Human.objects.filter(event=ev, accepted=True, revoked=False, is_creative=True))
 				ev.chef = random.choice(chefs).name
 				ev.save()
-			if places == 0 and ev.souschef is None:
+				mailworthy = True
+			if (places <= 0 or options['forcePick']) and ev.souschef is None:
 				souschefs = Human.objects.filter(event=ev, accepted=True, revoked=False).exclude(name=ev.chef)
 				ev.souschef = random.choice(souschefs).name
 				ev.save()
+				mailworthy = True
 
-			msg = ""
-			if len(new_names) == 1:
-				msg += "%s is ingeloot" % new_names[0]
-			else:
-				last = new_names.pop()
-				msg += "%s en %s zijn ingeloot" % (", ".join(new_names), last)
+			if not mailworthy:
+				continue
 
-			if len(already_accepted) == 1:
-				msg += " (naast %s)" % already_accepted[0]
-			elif len(already_accepted) > 1:
-				last = already_accepted.pop()
-				msg += " (naast %s en %s)" % (", ".join(already_accepted), last)
+			msgparts = []
+			if len(new_names) > 0:
+				msg = ""
+				if len(new_names) == 1:
+					msg += "%s is ingeloot" % new_names[0]
+				else:
+					last = new_names.pop()
+					msg += "%s en %s zijn ingeloot" % (", ".join(new_names), last)
+
+				if len(already_accepted) == 1:
+					msg += " (naast %s)" % already_accepted[0]
+				elif len(already_accepted) > 1:
+					last = already_accepted.pop()
+					msg += " (naast %s en %s)" % (", ".join(already_accepted), last)
+				msgparts.append(msg)
 
 			if places == 1:
-				msg += "\n\nEr is nog een plek. Meld je snel aan op:\nhttp://maandagseries.quis.cx"+ ev.get_absolute_url()
+				msgparts.append("Er is nog een plek. Meld je snel aan op:\nhttp://maandagseries.quis.cx"+ ev.get_absolute_url())
 			elif places > 1:
-				msg += "\n\nEr zijn nog %d plaatsen. Meld je snel aan op:\nhttp://maandagseries.quis.cx%s" % (places, ev.get_absolute_url())
+				msgparts.append("Er zijn nog %d plaatsen. Meld je snel aan op:\nhttp://maandagseries.quis.cx%s" % (places, ev.get_absolute_url()))
 
 			if not ev.chef is None:
-				msg += "\n\n%s mag een gerecht verzinnen." % ev.chef
+				msg = "%s mag een gerecht verzinnen." % ev.chef
 				if not ev.souschef is None:
 					msg += " %s mag helpen met boodschappen doen." % ev.souschef
+				msgparts.append(msg)
 
+			msg = "\n\n".join(msgparts)
 			headers = {'In-Reply-To': "<%s@maandagseries.quis.cx>" % ev.getKey(), 'References': "<%s@maandagseries.quis.cx>" % ev.getKey()}
 			email = EmailMessage('Re: %s' % ev.date.strftime('%e %b'), msg, 'maandagseries@karpenoktem.nl', ['maandagseries@karpenoktem.nl'], headers=headers)
 			email.send()
